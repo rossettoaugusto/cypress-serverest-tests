23e0c465b3547c827f7b002b3fd645f26602cb7a
Augusto Rossetto <augustorossetto3@gmail.com>
2025-12-03 15:20:16 -0300

    Ajustes Allure Report

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,32 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = defineConfig({
-  reporter: 'mochawesome',
+  reporter: "mochawesome",
   reporterOptions: {
-    reportDir: 'cypress/reports',
-    overwrite: false,
-    html: false,
-    json: true
+    reportDir: "cypress/reports",
+    overwrite: true,
+    html: true,
+    json: false,
+    reportFileName: "index",
+    inlineAssets: true,
+    charts: true,
+    timestamp: "shortDate",
   },
+
+  env: {
+    allure: true,
+    allureReuseAfterSpec: true,
+  },
+
   e2e: {
     baseUrl: "https://serverest.dev",
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: "cypress/support/e2e.js",
+
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config;
+    },
   },
 });
--- cypress/e2e/usuarios/listaUsuarios.cy.js
+++ cypress/e2e/usuarios/listaUsuarios.cy.js
@@ -1,35 +1,7 @@
-import { faker } from "@faker-js/faker";
-
-const baseUrl = "http://localhost:3000";
-
+import usuarios from "../../pages/usuarios/index.js";
 
 describe("Listagem de Usuarios", () => {
-
-    it("Listar usuarios cadastrados no sistema", () => {
-        cy.request({
-            url: `${baseUrl}/usuarios`,
-            method: "GET",
-        }).then((response) => {
-            expect(response.status).to.eq(200);
-            expect(response.body).to.have.property("quantidade");
-            expect(response.body).to.have.property("usuarios");
-            expect(response.body.usuarios).to.be.an("array");
-
-            const quantidade = response.body.quantidade;
-            const usuarios = response.body.usuarios;
-
-            cy.log(`Quantidade de usuarios: ${quantidade}`);
-            cy.log(`Usuarios: ${JSON.stringify(usuarios)}`);
-
-            if (usuarios.length > 0) {
-                const firstUser = usuarios[0];
-                expect(firstUser).to.have.property("nome");
-                expect(firstUser).to.have.property("email");
-                expect(firstUser).to.have.property("password");
-                expect(firstUser).to.have.property("administrador");
-                expect(firstUser).to.have.property("_id");
-            }
-
-        });
-    });
+  it("Listar usuarios cadastrados no sistema", () => {
+    usuarios.listarUsuarios();
+  });
 });
